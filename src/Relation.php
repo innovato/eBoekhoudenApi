9779dd85231e7aa2d3bbdc2cc240c1331a350c5c
Bob Kosse <bob@start-vandaag.nl>
2017-09-08 14:00:49 +0200

    Split relation object from relationValidator object

--- src/Relation.php
+++ src/Relation.php
@@ -2,49 +2,178 @@
 
 namespace bobkosse\eBoekhouden;
 
-class Relation
+use bobkosse\eBoekhouden\ObjectValidation\RelationValidator;
+
+/**
+ * Class Relation
+ * @package bobkosse\eBoekhouden
+ */
+class Relation extends RelationValidator
 {
-    private $id;                    // Mandatory
-    private $creationDate;
-    private $relationCode;
-    private $companyName;
-    private $contact;
-    private $sex;
-    private $address;
-    private $postalcode;
-    private $city;
-    private $country;
-    private $address2;
-    private $postalcode2;
-    private $city2;
-    private $country2;
-    private $phone;
-    private $mobile;
-    private $fax;
-    private $email;
-    private $website;
-    private $note;
-    private $bankacocunt;
-    private $giroaccount;
-    private $vatregistrationnumber;
-    private $preamble;
-    private $iban;
-    private $bic;
-    private $companyPerson = 'company';
-    private $freeField_1;
-    private $freeField_2;
-    private $freeField_3;
-    private $freeField_4;
-    private $freeField_5;
-    private $freeField_6;
-    private $freeField_7;
-    private $freeField_8;
-    private $freeField_9;
-    private $freeField_10;
-    private $memberAdministration = 0;
-    private $ledgerAccount_id;          // Mandatory
-    private $noEmail;                   // Mandatory
-    private $newsletterGroupCount;      // Mandatory
+    /**
+     * @var
+     */
+    protected $id;
+    /**
+     * @var
+     */
+    protected $creationDate;
+    /**
+     * @var
+     */
+    protected $relationCode;
+    /**
+     * @var
+     */
+    protected $companyName;
+    /**
+     * @var
+     */
+    protected $contact;
+    /**
+     * @var
+     */
+    protected $sex;
+    /**
+     * @var
+     */
+    protected $address;
+    /**
+     * @var
+     */
+    protected $postalcode;
+    /**
+     * @var
+     */
+    protected $city;
+    /**
+     * @var
+     */
+    protected $country;
+    /**
+     * @var
+     */
+    protected $address2;
+    /**
+     * @var
+     */
+    protected $postalcode2;
+    /**
+     * @var
+     */
+    protected $city2;
+    /**
+     * @var
+     */
+    protected $country2;
+    /**
+     * @var
+     */
+    protected $phone;
+    /**
+     * @var
+     */
+    protected $mobile;
+    /**
+     * @var
+     */
+    protected $fax;
+    /**
+     * @var
+     */
+    protected $email;
+    /**
+     * @var
+     */
+    protected $website;
+    /**
+     * @var
+     */
+    protected $note;
+    /**
+     * @var
+     */
+    protected $bankacocunt;
+    /**
+     * @var
+     */
+    protected $giroaccount;
+    /**
+     * @var
+     */
+    protected $vatregistrationnumber;
+    /**
+     * @var
+     */
+    protected $preamble;
+    /**
+     * @var
+     */
+    protected $iban;
+    /**
+     * @var
+     */
+    protected $bic;
+    /**
+     * @var string
+     */
+    protected $companyPerson = 'C';
+    /**
+     * @var
+     */
+    protected $freeField_1;
+    /**
+     * @var
+     */
+    protected $freeField_2;
+    /**
+     * @var
+     */
+    protected $freeField_3;
+    /**
+     * @var
+     */
+    protected $freeField_4;
+    /**
+     * @var
+     */
+    protected $freeField_5;
+    /**
+     * @var
+     */
+    protected $freeField_6;
+    /**
+     * @var
+     */
+    protected $freeField_7;
+    /**
+     * @var
+     */
+    protected $freeField_8;
+    /**
+     * @var
+     */
+    protected $freeField_9;
+    /**
+     * @var
+     */
+    protected $freeField_10;
+    /**
+     * @var int
+     */
+    protected $memberAdministration = 0;
+    /**
+     * @var
+     */
+    protected $ledgerAccount_id;
+    /**
+     * @var
+     */
+    protected $noEmail;
+    /**
+     * @var
+     */
+    protected $newsletterGroupCount;
 
     /**
      * @param mixed $id
@@ -456,6 +585,9 @@ class Relation
         return $this;
     }
 
+    /**
+     * @return array
+     */
     public function getEboekhoudenArray()
     {
         $this->validateFields();
@@ -503,104 +635,5 @@ class Relation
             "GeenEmail" => $this->noEmail,
             "NieuwsbriefgroepenCount" => $this->newsletterGroupCount,
         ];
-    }
-
-    private function validateFields()
-    {
-        $this->checkCreationDate();
-        $this->checkCompanyName();
-        $this->checkCompanyPerson();
-        $this->checkRelationCode();
-        $this->checkSex();
-        $this->checkFieldLength('address', $this->address, 150);
-        $this->checkFieldLength('postalcode', $this->postalcode, 50);
-        $this->checkFieldLength('city', $this->city, 50);
-        $this->checkFieldLength('country', $this->country, 50);
-        $this->checkFieldLength('address2', $this->address2, 100);
-        $this->checkFieldLength('postalcode2', $this->postalcode2, 50);
-        $this->checkFieldLength('city2', $this->city2, 50);
-        $this->checkFieldLength('country2', $this->country2, 50);
-        $this->checkFieldLength('phone', $this->phone, 50);
-        $this->checkFieldLength('mobile', $this->mobile, 50);
-        $this->checkFieldLength('fax', $this->fax, 50);
-        $this->checkFieldLength('email', $this->email, 150);
-        $this->checkFieldLength('vatregistrationnumber', $this->vatregistrationnumber, 50);
-        $this->checkFieldLength('preamble', $this->preamble, 50);
-        $this->checkFieldLength('iban', $this->iban, 50);
-        $this->checkFieldLength('bic', $this->bic, 50);
-        $this->checkFieldLength('freeField_1', $this->freeField_1, 100);
-        $this->checkFieldLength('freeField_2', $this->freeField_2, 100);
-        $this->checkFieldLength('freeField_3', $this->freeField_3, 100);
-        $this->checkFieldLength('freeField_4', $this->freeField_4, 100);
-        $this->checkFieldLength('freeField_5', $this->freeField_5, 100);
-        $this->checkFieldLength('freeField_6', $this->freeField_6, 100);
-        $this->checkFieldLength('freeField_7', $this->freeField_7, 100);
-        $this->checkFieldLength('freeField_8', $this->freeField_8, 100);
-        $this->checkFieldLength('freeField_9', $this->freeField_9, 100);
-        $this->checkFieldLength('freeField_10', $this->freeField_10, 100);
-        $this->checkFieldLength('ledgerAccount_id', $this->ledgerAccount_id, 100, true);
-        $this->checkFieldLength('noEmail', $this->noEmail, 100, true);
-        $this->checkFieldLength('newsletterGroupCount', $this->newsletterGroupCount, 100, true);
-    }
-
-    private function checkCreationDate()
-    {
-        if($this->creationDate === null || $this->creationDate === '') {
-            $this->creationDate = date("Y-m-d");
-        }
-    }
-
-    private function checkCompanyName()
-    {
-        if($this->companyName === null || $this->companyName === '') {
-            if($this->companyPerson === 'person') {
-                $this->companyName = $this->contact;
-            } else {
-                $this->companyName = "UNKNOWN";
-            }
-        }
-
-        $this->checkFieldLength('companyName', $this->companyName, 100);
-    }
-
-    private function checkCompanyPerson()
-    {
-        if($this->companyPerson === 'person') {
-            $this->contact = '';
-            $this->companyPerson = 'P';
-            return;
-        }
-        $this->companyPerson = 'P';
-        return;
-    }
-
-    private function checkRelationCode()
-    {
-        if($this->relationCode === null || $this->relationCode === '') {
-            $this->relationCode = substr($this->companyName, 0, 8) . date('mY');
-        }
-
-        $this->checkFieldLength('realtionCode', $this->relationCode, 15);
-    }
-
-    private function checkSex()
-    {
-        $acceptedvalues = ['M', 'm', 'V', 'v', ''];
-        if(!in_array($this->sex, $acceptedvalues)) {
-            throw new \Exception('Sex must be empty string, null, M or V', 112);
-        }
-    }
-
-    private function checkFieldLength($fieldName, $fieldValue, $maxLenght, $isRequired = false)
-    {
-        if(strlen($fieldValue) > $maxLenght) {
-            throw new \Exception('Field ' . $fieldName . ' may not exceed ' . $maxLenght . ' characters', 118);
-        }
-
-        if($isRequired) {
-            if ($fieldValue == '' || $fieldValue == null) {
-                throw new \Exception('Field ' . $fieldName . ' is a required field', 118);
-            }
-        }
     }
 }
